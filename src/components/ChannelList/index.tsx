f7ab4e07fa1a7306bdf19a433e8f8f727ee392cf
Pedro Lucas <pedro.nunes1432@gmail.com>
2020-10-13 16:11:38 -0300

    Adicionado ChannelButton, ChannelList, UserInfo

--- src/components/ChannelList/index.tsx
+++ src/components/ChannelList/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import { Container, Category, AddCategoryIcon } from "./styles";
+
+import ChannelButton from '../ChannelButton';
 
 const ChannelList: React.FC = () => {
   return (
-      <Container>
-          
-      </Container>
+    <Container>
+      <Category>
+        <span>Canais de texto</span>
+        <AddCategoryIcon />
+      </Category>
+
+      <ChannelButton channelName="Geral" />
+      <ChannelButton channelName="Adicionar-músicas" />
+      <ChannelButton channelName="League of Legends" />
+      <ChannelButton channelName="Among us" />
+    </Container>
   );
-}
+};
 
 export default ChannelList;
---
+++ src/components/UserInfo/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  
+`;
+
+export const Profile = styled.div``;
+
+export const Avatar = styled.div``;
+
+export const UserData = styled.div``;
+
+export const Icons = styled.div``;
+
+export const MicIcon = styled.div``;
+
+export const HeadphoneIcon = styled.div``;
+
+export const SettingsIcon = styled.div``;
